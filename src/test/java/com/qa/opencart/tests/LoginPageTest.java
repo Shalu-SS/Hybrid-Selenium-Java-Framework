d95c6d5264ba6e0cc0716240fb253223638d8bb6
Shalu-SS <s.shaluss@gmail.com>
2023-10-02 16:35:58 +0530

    Project issue fixed

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -10,12 +10,13 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
-@Epic("EPIC - 100 : Design Login Page for Open cart Application...")
-@Story("US - 101: Login Page Features")
+
+//@Epic("EPIC - 100 : Design Login Page for Open cart Application...")
+//@Story("US - 101: Login Page Features")
 public class LoginPageTest extends BaseTest{
 
-	@Description("login Page Title Test")
-	@Severity(SeverityLevel.MINOR)
+//	@Description("login Page Title Test")
+//	@Severity(SeverityLevel.MINOR)
 	@Test
 	public void loginPageTitleTest() {
 		String title = loginpage.getLoginPageTitle();
@@ -24,8 +25,8 @@ public class LoginPageTest extends BaseTest{
 	}
 
 
-	@Description("login Page URL Test")
-	@Severity(SeverityLevel.NORMAL)
+//	@Description("login Page URL Test")
+//	@Severity(SeverityLevel.NORMAL)
 	@Test
 	public void loginPageURLTest() {
 		String url = loginpage.getLoginUrl();
@@ -33,15 +34,15 @@ public class LoginPageTest extends BaseTest{
 		Assert.assertTrue(url.contains(Constants.LOGIN_PAGE_URL_FRACTION));
 	}
 	
-	@Description("login Page Forgot Pwd Link Exits Test")
-	@Severity(SeverityLevel.CRITICAL)
+//	@Description("login Page Forgot Pwd Link Exits Test")
+//	@Severity(SeverityLevel.CRITICAL)
 	@Test
 	public void ForgotPwdTest() {
 		Assert.assertTrue(loginpage.isForgotPwdLinkExits());
 	}
 	
-	@Description("Positive test case for login with credentials...")
-	@Severity(SeverityLevel.BLOCKER)
+//	@Description("Positive test case for login with credentials...")
+//	@Severity(SeverityLevel.BLOCKER)
 	@Test(description = "Positive test case for login with credentials...")
 	public void loginTest() {
 		loginpage.doLogin(prop.getProperty("username").trim(), prop.getProperty("password").trim());
